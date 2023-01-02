388dc6714b33958376e30120fe9c6db31fee79ae
Kermit <kermit_sibindi@live.com>
2023-01-02 20:48:47 +0000

    added animal choices

--- _03_general/hellopy.py
+++ _03_general/hellopy.py
@@ -1,1 +1,9 @@
-print("Hello Kermit")
+animals = []
+no_of_animals = int(input("How many animals do you have?"))
+x = 0;
+while (x < no_of_animals):
+    animals.insert(x, input("Enter animal type eg lion "))
+    x+=1
+
+
+print(animals)
