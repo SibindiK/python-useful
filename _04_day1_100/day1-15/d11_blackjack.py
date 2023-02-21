e6321f451787d121abb03d4ae08adcfb9992564f
SibindiK <kermit_sibindi@live.com>
2023-02-21 09:12:49 +0000

    moved game and display logic to own functions

--- _04_day1_100/day1-15/d11_blackjack.py
+++ _04_day1_100/day1-15/d11_blackjack.py
@@ -1,3 +1,13 @@
+#################### BLACK JACK RULES #######################
+# The deck is unlimited size
+# There are no jokers
+# The Jack/Queen/King all count as 10
+# The Ace can count as 11 or 1
+# All cards have equal probability of being drawn
+# Cards are not removed from the deck after being drawn
+#
+#############################################################
+
 import random
 
 
@@ -15,9 +25,48 @@ def total_cards(cards_picked, cards_deck):
     return total
 
 
+def show_result(result, player_cards, dealer_cards, cards_deck):
+    '''display final result on screen'''
+    print(
+        f"You {result}, your final hand {player_cards}, score {total_cards(player_cards, cards_deck)}")
+    print(
+        f"Dealer's final hand: {dealer_cards}, score {total_cards(dealer_cards, cards_deck)}")
+
+
+def play_black_jack(cards_deck, player_cards, dealer_cards):
+    '''play the black jack game'''
+    player_cards.append(get_card(cards_deck))
+    player_cards.append(get_card(cards_deck))
+    print(
+        f"Your cards are [{player_cards[0]}] [{get_card(player_cards[1])}] ")
+    dealer_cards.append(get_card(cards_deck))
+    print(f"Dealer open card is [{dealer_cards[0]}] ")
+    another_card = True
+    count = 2
+    while another_card:
+        draw_again = input("Type'd' to draw again or 's' to stand: ")
+        if draw_again == 'd':
+            player_cards.append(get_card(cards_deck))
+            print(f"You have drawn [{player_cards[-1]}]")
+            count += 1
+        elif draw_again == 's':
+            dealer_cards.append(get_card(cards_deck))
+            print(f"Dealer second card is [{dealer_cards[-1]}]")
+            player_total = total_cards(player_cards, cards_deck)
+            dealer_total = total_cards(dealer_cards, cards_deck)
+            if player_total > dealer_total and player_total < 22:
+                show_result("WIN", player_cards, dealer_cards, cards_deck)
+            elif player_total < dealer_total or player_total > 21:
+                show_result("LOSE", player_cards, dealer_cards, cards_deck)
+            elif player_total == dealer_total and player_total < 21:
+                show_result("DRAW", player_cards, dealer_cards, cards_deck)
+
+            another_card = False
+
+
 def main():
     deck = {
-        'A': 1,
+        'A': 11,
         '2': 2,
         '3': 3,
         '4': 4,
@@ -32,33 +81,16 @@ def main():
         'K': 10,
     }
 
-    player_cards = []
-    dealer_cards = []
-    player_cards.append(get_card(deck))
-    player_cards.append(get_card(deck))
-    print(f"Your cards are [{player_cards[0]}] [{get_card(player_cards[1])}] ")
-    dealer_cards.append(get_card(deck))
-    print(f"Dealer open card is [{dealer_cards[0]}] ")
-    another_card = True
-    count = 2
-    while another_card:
-        draw_again = input("Type'd' to draw again or 's' to stand: ")
-        if draw_again == 'd':
-            player_cards.append(get_card(deck))
-            print(f"You have drawn [{player_cards[-1]}]")
-            count += 1
-        elif draw_again == 's':
-            dealer_cards.append(get_card(deck))
-            print(f"Dealer second card is [{dealer_cards[-1]}]")
-            player_total = total_cards(player_cards, deck)
-            dealer_total = total_cards(dealer_cards, deck)
-            if player_total > dealer_total:
-                print(
-                    f"You WIN, your cards total {player_total} and Dealer's cards total {dealer_total}")
-            elif player_total < dealer_total:
-                print(
-                    f"You LOSE, your cards total {player_total} and Dealer's cards total {dealer_total}")
-            another_card = False
+    another_game = True
+    while another_game:
+        play_game = input(
+            "Do you want to play a game of BlackJack? Type 'y' or 'n': ").lower()
+        if play_game == 'y':
+            player_cards = []
+            dealer_cards = []
+            play_black_jack(deck, player_cards, dealer_cards)
+        elif play_game == 'n':
+            another_game = False
 
 
 if __name__ == "__main__":
