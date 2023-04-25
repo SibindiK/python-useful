76c1a30be635bb4da3285f0b80af00ca772e2665
SibindiK <kermit_sibindi@live.com>
2023-04-25 08:47:21 +0100

    pong game completed - scoreboard and ball speed increment

--- _04_day1_100/day22/main.py
+++ _04_day1_100/day22/main.py
@@ -4,6 +4,7 @@ import time
 
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 
 def create_screen():
@@ -17,10 +18,13 @@ def create_screen():
 
 def main():
     game_is_on = True
+    ball_in_play = True
+
     screen = create_screen()
     r_pad = Paddle(350, 0)
     l_pad = Paddle(-350, 0)
     ball = Ball()
+    scoreboard = Scoreboard()
     screen.listen()
     screen.onkey(r_pad.go_up, "Up")
     screen.onkey(r_pad.go_down, "Down")
@@ -28,7 +32,7 @@ def main():
     screen.onkey(l_pad.go_down, "s")
 
     while game_is_on:
-        time.sleep(0.1)
+        time.sleep(ball.ball_speed)
         screen.update()
         ball.move()
 
@@ -36,10 +40,20 @@ def main():
         if ball.ycor() > 330 or ball.ycor() < -330:
             ball.bounce_y()
 
-        #detect collision with the paddles
+        # detect collision with the paddles
         if ball.distance(r_pad) < 50 and ball.xcor() > 320 or \
                 ball.distance(l_pad) < 50 and ball.xcor() < -320:
             ball.bounce_x()
+
+        # detect if ball has been missed by right pad
+        if ball.xcor() > 380:
+            ball.reset_ball()
+            scoreboard.inc_l_score()
+        # detect if ball has been missed by left pad
+        if ball.xcor() < -380:
+            ball.reset_ball()
+            scoreboard.inc_r_score()
+
     screen.exitonclick()
 
 
